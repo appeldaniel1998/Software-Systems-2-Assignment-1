ed9e1096afcff7f240c2b10cf40b65656553b775
Daniel Appel <45691773+appeldaniel1998@users.noreply.github.com>
2022-03-07 23:18:15 +0200

    Upload part 1 of the assignment

---
+++ mat.cpp
@@ -0,0 +1,11 @@
+#include "mat.hpp"
+#include <string>
+using namespace std;
+
+namespace ariel
+{
+    string mat(int a, int b, char ch1, char ch2)
+    {
+        return &ch1;
+    }
+}
---
+++ mat.hpp
@@ -0,0 +1,7 @@
+#include <string>
+using namespace std;
+
+namespace ariel
+{
+    string mat(int a, int b, char ch1, char ch2);
+}
